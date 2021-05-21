3a702cecc5fdfa99160dec0c7a4b3e821534dd7f
Tomás <t.sarmiento01@ufromail.cl>
2021-05-21 18:26:08 -0400

    added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,32 +1,40 @@
 import React, { useState } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import Leaderboard from "./Leaderboard";
+import Leaderboard from "../leaderboard/Leaderboard";
+import Transition from "../Transition";
+import Chart from "../charts/Chart";
 
 const Home = () => {
   //const [user, setUser] = useState(""); to be implemented when this is a functional component
 
   return (
-    <div className="container_home">
-      <div className="upper">
-        <p>THE INSPECTORATE GAMES</p>
-      </div>
-      <div className="middle">
-        <div className="leaderboard">
-          <Leaderboard />
+    <Transition>
+      <div className="container_home">
+        <div className="upper">
+          <p>THE INSPECTORATE GAMES</p>
         </div>
-        <div className="leaderboard-right">
-          <h1>Welcome, user!</h1>
-          <Link className="btn-link" to="/takeChallenge">
-            Take challenge
-          </Link>
-          <Link className="btn-link">View past challenges</Link>
+        <div className="middle">
+          <div className="leaderboard">
+            <Leaderboard />
+          </div>
+          <div className="leaderboard-right">
+            <h1>Welcome, user!</h1>
+            <Link className="btn-link" to="/takeChallenge">
+              Take challenge
+            </Link>
+            <Link className="btn-link" to="/home">
+              View past challenges
+            </Link>
+            <div className="chart">
+              <h2>Challenges completed this week</h2>
+              <Chart className="chart" />
+            </div>
+          </div>
         </div>
+        <div className="bottom"></div>
       </div>
-      <div className="bottom">
-        <Link className="signout btn-link">Sign out</Link>
-      </div>
-    </div>
+    </Transition>
   );
 };
 
