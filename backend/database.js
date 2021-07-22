76df0a48da4fea3635724bfcfff887fa7580d2d4
PappyMint <fyy7680@autuni.ac.nz>
2021-07-23 10:15:38 +1200

    fixed backend

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const connection = process.env.DB_CONN;
+const connection =
+  "mongodb+srv://admin:3pm3iquJ11JnachM@tigcluster.u3x5p.mongodb.net/TIGCluster?retryWrites=true&w=majority";
+// const connection = process.env.DB_CONN;
 mongoose
   .connect(connection, {
     useNewUrlParser: true,
