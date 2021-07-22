7a66cab3dbdeb1b5d47dca6502eded3c3261cc4f
PappyMint <fyy7680@autuni.ac.nz>
2021-07-23 08:40:19 +1200

    added the backend folders

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import LoginMain from "./LoginMain";
-import "./styles/styles.scss";
-
-ReactDOM.render(<LoginMain />, document.getElementById("root"));
