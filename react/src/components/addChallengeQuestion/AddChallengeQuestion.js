247def20a5ac5f2ade9a10e45177a02e1ca3cd4a
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2021-08-10 23:39:40 -0400

    Desktop version created, minor changes in Home.css

--- react/src/components/addChallengeQuestion/AddChallengeQuestion.js
+++ react/src/components/addChallengeQuestion/AddChallengeQuestion.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Transition from "../Transition";
 
 const AddChallengeQuestion = () => {
   const addChallengeQuestion = (challengeName) => {
@@ -18,33 +19,116 @@ const AddChallengeQuestion = () => {
   };
 
   return (
-    <>
-      <header>
-        <a href="/admindashboard">Left arrow</a>
-
-        <h1>Add Challenge Question</h1>
-      </header>
-      <section>
-        <article>
-          <button onClick={() => addChallengeQuestion("MCQ")}>MCQ</button>
-        </article>
-        <article>
-          <button onClick={() => addChallengeQuestion("Fill in blank")}>
-            Fill in blank
-          </button>
-        </article>
-        <article>
-          <button onClick={() => addChallengeQuestion("Find in picture")}>
-            Find in picture
-          </button>
-        </article>
-        <article>
-          <button onClick={() => addChallengeQuestion("Answer a question")}>
-            Answer a question
-          </button>
-        </article>
-      </section>
-    </>
+    <Transition>
+      <main className="addChallengeQuestionComponent">
+        <a href="/admindashboard">
+          <img src="./resources/goback.png" alt="Go Back" />
+        </a>
+        <header>
+          <h1>Add Challenge Question</h1>
+        </header>{" "}
+        <section>
+          <article>
+            <img
+              src="./resources/MCQ.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "2", "--j": "0" }}
+            />
+            <img
+              src="./resources/MCQ.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "1", "--j": "1" }}
+            />
+            <img
+              src="./resources/MCQ.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "0", "--j": "2" }}
+            />
+            <img
+              src="./resources/MCQ.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "-1", "--j": "3" }}
+            />
+            <button onClick={() => addChallengeQuestion("MCQ")}>MCQ</button>
+          </article>
+          <article>
+            <img
+              src="./resources/fillinblank.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "2", "--j": "0" }}
+            />
+            <img
+              src="./resources/fillinblank.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "1", "--j": "1" }}
+            />
+            <img
+              src="./resources/fillinblank.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "0", "--j": "2" }}
+            />
+            <img
+              src="./resources/fillinblank.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "-1", "--j": "3" }}
+            />
+            <button onClick={() => addChallengeQuestion("Fill in blank")}>
+              Fill in blank
+            </button>
+          </article>
+          <article>
+            <img
+              src="./resources/Findinpicture.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "2", "--j": "0" }}
+            />
+            <img
+              src="./resources/Findinpicture.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "1", "--j": "1" }}
+            />
+            <img
+              src="./resources/Findinpicture.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "0", "--j": "2" }}
+            />
+            <img
+              src="./resources/Findinpicture.png"
+              alt="Multiple Choice Question"
+              style={{ "--i": "-1", "--j": "3" }}
+            />
+            <button onClick={() => addChallengeQuestion("Find in picture")}>
+              Find in picture
+            </button>
+          </article>
+          <article>
+            <img
+              src="./resources/answeraquestion.webp"
+              alt="Multiple Choice Question"
+              style={{ "--i": "2", "--j": "0" }}
+            />
+            <img
+              src="./resources/answeraquestion.webp"
+              alt="Multiple Choice Question"
+              style={{ "--i": "1", "--j": "1" }}
+            />
+            <img
+              src="./resources/answeraquestion.webp"
+              alt="Multiple Choice Question"
+              style={{ "--i": "0", "--j": "2" }}
+            />
+            <img
+              src="./resources/answeraquestion.webp"
+              alt="Multiple Choice Question"
+              style={{ "--i": "-1", "--j": "3" }}
+            />
+            <button onClick={() => addChallengeQuestion("Answer a question")}>
+              Answer a question
+            </button>
+          </article>
+        </section>
+      </main>
+    </Transition>
   );
 };
 
